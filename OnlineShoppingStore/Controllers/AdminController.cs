ef57cb309574b834b2ac64a44335a290df6a6009
Keith S. Safford <ks.safford@verizon.net>
2021-05-30 16:13:55 -0400

    Part-6: Ecommerce Dashboard or Admin using Bootstrap, CSS, ASP.NET MVC

---
+++ OnlineShoppingStore/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineShoppingStore.Context;
+using OnlineShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public UnitOfWork _unitOfWork = new UnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult Categories()
+        {
+            List<Category> allcategories = _unitOfWork
+                .GetRepositoryInstance<Category>()
+                .GetAllRecordsIQueryable()
+                .Where(i => i.IsDelete == false)
+                .ToList();
+
+            return View(allcategories);
+        }
+    }
+}
