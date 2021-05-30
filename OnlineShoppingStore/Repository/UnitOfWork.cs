c7c95511398384d812856847b59f9a9c86c5bb43
Keith S. Safford <ks.safford@verizon.net>
2021-05-30 15:52:16 -0400

    Part-5: Repository pattern in eCommerce project, ASP.NET MVC, jQuery with AJAX and JSON

---
+++ OnlineShoppingStore/Repository/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingStore.Context;
+
+namespace OnlineShoppingStore.Repository
+{
+    public class UnitOfWork : IDisposable
+    {
+        private OnlineShoppingStoreEntities _db = new OnlineShoppingStoreEntities();
+        public IRepository<Entity> GetRepositoryInstance<Entity>() where Entity : class
+        {
+            return new Repository<Entity>(_db);
+        }
+
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+    }
+}
